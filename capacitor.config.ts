b5c3f4f9665cec6bbccd1aea8ba3cc78a0882ca6
welly830 <welly830@gmail.com>
2023-03-20 17:41:15 -0300

    agoravai

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'appteste02',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
