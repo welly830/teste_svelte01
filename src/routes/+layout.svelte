9c9e93a8e4193325ac8bc4ee07b2880c2cbd7c92
welly830 <welly830@gmail.com>
2023-03-20 15:13:29 -0300

    modificacoes_pwa1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+  import Pwa from "$lib/Pwa.svelte";
   import "../app.postcss";
   import BottomNav from "../lib/BottomNav.svelte";
   import Header from "../lib/Header.svelte";
 </script>
 <Header/>
+<Pwa/>
 <BottomNav/>
 <slot />
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [VitePWA(
+		{
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
+            },
+            registerType: "autoUpdate",
+            manifest: {
+                "background_color": "#ffffff",
+                "theme_color": "#7E1F86",
+                "name": "Unixfy.net",
+                "short_name": "Unixfy.net",
+                "start_url": "/",
+                "display": "standalone",
+                "icons": [
+                    {
+                        "src": "/images/largeicon.png",
+                        "sizes": "1000x1000",
+                        "type": "image/png",
+                        "purpose": "maskable any"
+                    }
+                ]
+            }
+        }
+	)
+	,	sveltekit()
+	]
 });
