9d9cee2a2cfb52a4433c8daa9e44d4121a778a5c
welly830 <welly830@gmail.com>
2023-03-20 14:28:29 -0300

    navigation_bar

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<nav class="bg-white shadow dark:bg-gray-700">
+    <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+         
+        
+        
+    </div>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../app.postcss";
+  import BottomNav from "../lib/BottomNav.svelte";
+  import Header from "../lib/Header.svelte";
+</script>
+<Header/>
+<BottomNav/>
+<slot />
